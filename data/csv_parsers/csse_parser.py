a567195740a9e01fa22d0be2222f8bab7543a3c6
Spacedoge <>
2020-05-01 23:32:03 +0300

    Parser bug fix

--- data/csv_parsers/csse_parser.py
+++ data/csv_parsers/csse_parser.py
@@ -9,30 +9,27 @@ class CSSEParser:
         self.downloader = ReportDownloader(self.cfg)
         self.data = [
             (self.cfg["global_confirmed"], "world_timeseries_confirmed.csv"),
-            (self.cfg["global_recovered"], "world_timeseries_recovered.csv"),
             (self.cfg["global_deaths"], "world_timeseries_deaths.csv"),
         ]
 
     def _compose(self, frames):
-        frames = [frame.drop(['Lat', 'Long'], 1).groupby("Country/Region").sum() for frame in frames]
-        assert frames[0].shape == frames[1].shape == frames[2].shape
-        raw_data = np.stack([frame.values for frame in frames], -1)
-        return frames[0].index, frames[0].columns[2:], raw_data
+        frames = [
+            pd.melt(
+                frame.drop(['Province/State', 'Lat', 'Long'], 1),
+                id_vars=['Country/Region'],
+                var_name='date',
+                value_name='val'
+            ).set_index(['Country/Region', 'date'])
+            for frame in frames
+        ]
+        frames = pd.concat(frames, axis=1).reset_index()
+        return frames
 
     def load_data(self):
         frames = [
             self.downloader.download_report(page, fname) for page, fname in self.data
         ]
-        countries, dates, values = self._compose(frames)
-
-        csv_data = []
-        for country_idx, country_name in enumerate(countries):
-            for date_idx, date in enumerate(dates):
-                csv_data.append(
-                    [date, country_name] + list(values[country_idx][date_idx])
-                )
-        csv_data = pd.DataFrame(
-            csv_data, columns=["date", "country_code", "cases", "deaths", "recovered"]
-        )
-        csv_data = fix_date(csv_data, "%m/%d/%y")
+        frames = self._compose(frames)
+        frames.columns=["country_code", "date", "cases", "deaths"]
+        csv_data = fix_date(frames, "%m/%d/%y").sort_values(by=['country_code', 'date'])
         return csv_data
